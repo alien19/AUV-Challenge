527e14667a107002edf46527aa5e3da40d2b6b94
EslamAsfour <40435240+EslamAsfour@users.noreply.github.com>
2020-11-04 00:14:14 +0200

    Sob7an Allaah <3

--- Detect.py
+++ Detect.py
@@ -7,7 +7,7 @@ import numpy as np
 
 x =1
 
-for i in range(x,x+20):
+for i in range(x,x+500):
     #print("E:\Courses\OpenCV Learning\Test Cases\\"+ str(i) + ".jpg" )
     frame = cv2.imread("E:\Courses\OpenCV Learning\Test Cases\\"+ str(i) + ".jpg" ,-1)
     
@@ -39,9 +39,16 @@ for i in range(x,x+20):
     """ 
   
     frame2 = frame
-    cntsSorted = sorted(contours, key=lambda x: cv2.arcLength(x,False))
+    cntsSorted = sorted(contours, key=lambda x: cv2.arcLength(x,True))
     print("Number of contours = " + str(len(contours)), len(contours[-1]))
-    cv2.drawContours(frame, cntsSorted[-3], -1, (0, 255, 0), 3)
+    
+    cv2.drawContours(frame, cntsSorted[-1], -1, (0, 0, 0), 3)
+    
+    
+    (x, y, w, h) = cv2.boundingRect(cntsSorted[-1])
+    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255),2)
+    
+    cv2.drawContours(frame, cntsSorted[-9:-3], -1, (0, 255, 0), 3)
     #cv2.drawContours(frame2, contours, -1, (0, 255, 0), 3)
     cv2.imshow("Img" , frame)
     cv2.imshow("Img2" , frame2)
