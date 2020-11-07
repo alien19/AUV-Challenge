385d6cd53e84a662a28581b64ec709795c58c4a6
EslamAsfour <40435240+EslamAsfour@users.noreply.github.com>
2020-11-07 23:46:30 +0200

    Some Tuning and need to get line intersection

--- Detect.py
+++ Detect.py
@@ -1,5 +1,4 @@
 # My Code 
-
 import math
 import cv2
 import numpy as np
@@ -17,73 +16,90 @@ def BotPointThenUpper (Line):
 
 
 
-x =6
+x =1
 
 for NIMG in range(x,x+500):
-    #print("E:\Courses\OpenCV Learning\Test Cases\\"+ str(i) + ".jpg" )
+    print("E:\Courses\OpenCV Learning\Test Cases\\"+ str(NIMG) + ".jpg" )
+    
     frame = cv2.imread("E:\Courses\OpenCV Learning\Test Cases\\"+ str(NIMG) + ".jpg" ,-1)
     Original = np.copy(frame)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    median = cv2.medianBlur(gray,5)
     
-    ret = cv2.adaptiveThreshold(median,255 ,cv2.ADAPTIVE_THRESH_MEAN_C , cv2.THRESH_BINARY , 9,2)
-    
-
-    kernel = np.ones((2,2),np.uint8)
-
-    edges = cv2.Canny(ret, 10, 20)
+    cv2.imshow("Original" , frame)
+    ret = cv2.adaptiveThreshold(gray,255 ,cv2.ADAPTIVE_THRESH_MEAN_C , cv2.THRESH_BINARY , 9,2)
+    median = cv2.medianBlur(ret,5)
+    kernel= np.ones((5,5),np.float32)/25
+    huh = cv2.filter2D(median,-1 , kernel)
+    edges = cv2.Canny(huh, 10, 20)
  
-    erosion = cv2.dilate(edges,kernel,iterations = 1)
+ #Mlhaash lzma
+   # erosion = cv2.dilate(edges,kernel,iterations = 1)
     
  
     # Using the Canny filter to get contours
-    contours, hierarchy = cv2.findContours(erosion, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours2, hierarchy2 = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-  
-    """
-        for contour in contours:
-                (x, y, w, h) = cv2.boundingRect(contour)
-
-                if cv2.contourArea(contour) <200:
-                    continue
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0),2)
-                
-    """ 
-  
-    # Sort By Lenght
-    cntsSorted = sorted(contours, key=lambda x: cv2.arcLength(x,True))
     
-    print("Number of contours = " + str(len(contours)),)
+    tempContour =  []
+
+    for contour in contours:
+                (x, y, w, h) = cv2.boundingRect(contour)
+                cv2.drawContours(frame,contour, -1, (0,255, 0),1)
+                cv2.imshow('Line',frame)
+                # We Filter the Contour with the needed Ones
+                if h*3 < w and w > 100  and h < 80 and h > 10:
+                    tempContour.append(contour)
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 255),2)
+                    
+
+
+    # Sort By Lenght
+    print(len(tempContour))
+    
+    if len(tempContour) == 0 :
+        print("Hahahahahahha")
+        continue
+        
+
+    cntsSorted = sorted(tempContour, key=lambda x: cv2.arcLength(x,True))
+    (x, y, w, h) = cv2.boundingRect(cntsSorted[-1])
+    cv2.rectangle(frame, (x, y), (x+w, y+h), (100, 150, 255),2)
+    
     
     #Draw The Longest Contour
-   # cv2.drawContours(frame, cntsSorted[-1], -1, (0, 0, 0), 3)
- 
  
     # then apply fitline() function
     [vx,vy,x,y] = cv2.fitLine(cntsSorted[-1],cv2.DIST_L2,0,0.01,0.01)
 
-# Now find two extreme points on the line to draw line
+    # Now find two extreme points on the line to draw line
     lefty = int((-x*vy/vx) + y)
     righty = int(((gray.shape[1]-x)*vy/vx)+y)
 
   #  (x, y, w, h) = cv2.boundingRect(cntsSorted[-1])
     #Finally draw the line
     cv2.line(frame,(gray.shape[1]-1,righty),(0,lefty),(0,0,0),1)
-    
+    cv2.imshow("AGHAHA" , frame)
+   
     cv2.imshow('Line',frame)
-    
+ 
     ######################################## 
+    offset = righty-5
+    if offset <0 : 
+        offset = 0 
     
-    
-    CheckUnderLine = gray[righty-5: , : ]
-    
+    CheckUnderLine = gray[offset: , : ]
+    cv2.imshow('Lin22211e',CheckUnderLine)
     ###############################################################################################################################
     
     edges22 = cv2.Canny(CheckUnderLine,10,20,apertureSize = 3)
    
    
-    lines = cv2.HoughLinesP(image=edges22,rho=1,theta=np.pi/180, threshold=50,lines=np.array([]), minLineLength=100,maxLineGap=90)
+    lines = cv2.HoughLinesP(image=edges22,rho=1,theta=np.pi/180, threshold=50, minLineLength=100,maxLineGap=90)
+    if lines is None:
+        print("Hhaahha")
+        print("A = " + str(a))
     a,b,c = lines.shape
+    print("A = " + str(a))
     Vert_Line = np.zeros((2,4) , dtype='i')
     TempLine = np.zeros((1,4))
     RefLine = np.zeros((1,4))
@@ -92,7 +108,9 @@ for NIMG in range(x,x+500):
     for i in range(a):
         # Search For the Vertical Lines 
         # We Sum up all the Possible X,Y coords and then we take the Avg 
-        if abs(lines[i][0][1] - lines[i][0][3]) > 5 and abs(lines[i][0][0] - lines[i][0][2]) < 20:
+        #cv2.line(frame, (lines[i][0][0] , lines[i][0][1]), (lines[i][0][2] , lines[i][0][3]), (0, 255, 255), 3, cv2.LINE_AA)
+
+        if abs(lines[i][0][1] - lines[i][0][3]) > 5 and abs(lines[i][0][0] - lines[i][0][2]) < 60:
             count = count+1
             TempLine = BotPointThenUpper(lines[i][0])
               # First Line 
@@ -112,17 +130,19 @@ for NIMG in range(x,x+500):
                 Vert_Line[0] =  Vert_Line[0] +TempLine
                 CountVert1 = CountVert1 + 1
             
-            
-  
+    print("_______________________________")
+    print(count)
+    if CountVert1 == 0 or CountVert2 == 0:
+        print("Something is Wronge")
  #Create The best two Vertical lines based on the avg
     Vert_Line[0] = (Vert_Line[0]/CountVert1)            
     Vert_Line[1] = (Vert_Line[1]/CountVert2)
 # 1/5 of the total Width
-    Width = math.ceil(abs(Vert_Line[0][0] - Vert_Line[1][0]) / 5 )   
+    Width = math.ceil(abs(Vert_Line[0][0] - Vert_Line[1][0]) *  .2 )   
     if Vert_Line[0][0] >  Vert_Line[1][0] : 
-        cv2.line(frame, (Vert_Line[1][0], Vert_Line[1][1]+righty-5), (Vert_Line[1][0] + Width, Vert_Line[1][1]+righty-5), (255, 255, 255), 3, cv2.LINE_AA)
+        cv2.line(frame, (Vert_Line[1][0], math.ceil(abs(Vert_Line[1][1] + Vert_Line[1][3] )/2)+righty-5), (Vert_Line[1][0] + Width, math.ceil(abs(Vert_Line[1][1] + Vert_Line[1][3] )/2)+righty-5), (255, 255, 255), 3, cv2.LINE_AA)
     else : 
-        cv2.line(frame, (Vert_Line[0][0], Vert_Line[0][1]+righty-5), (Vert_Line[0][0] + Width, Vert_Line[0][1]+righty-5), (255, 255, 255), 3, cv2.LINE_AA)
+        cv2.line(frame, (Vert_Line[0][0], math.ceil(abs(Vert_Line[0][1] + Vert_Line[0][3] )/2)+righty-5), (Vert_Line[0][0] + Width, math.ceil(abs(Vert_Line[0][1] + Vert_Line[0][3] )/2)+righty-5), (255, 255, 255), 3, cv2.LINE_AA)
      
     # 2 Vertical Lines    
     cv2.line(frame, (Vert_Line[0][0], Vert_Line[0][1]+righty-5), (Vert_Line[0][2], Vert_Line[0][3]+righty-5), (0, 0, 255), 3, cv2.LINE_AA)
@@ -150,3 +170,5 @@ for NIMG in range(x,x+500):
     cv2.destroyAllWindows()
 
 cv2.destroyAllWindows()
+
+
--- Test.py
+++ Test.py
@@ -12,10 +12,37 @@ def GetBotPoint (Line):
 
 
 
+
+def intersection( o1,  p1,  o2,  p2,
+                       r):
+
+    x = o2 - o1
+    d1 = p1 - o1
+    d2 = p2 - o2
+
+    cross = d1.x*d2.y - d1.y*d2.x
+    if abs(cross) < 1e-8 : 
+        return False
+
+    t1 = (x.x * d2.y - x.y * d2.x)/cross
+    r = o1 + d1 * t1
+    return True
+
+
+img = cv2.imread("E:\Courses\OpenCV Learning\Test Cases\\"+ str(2) + ".jpg" ,-1)
 x,y = 1,1
 Point1 = np.zeros((2,2))
 Point2 = np.ones((1,2))
 Point1[0] = [1,2]
 print(Point1[0] +Point2 )
-
-
+p1 = [20,20]
+o1  = [150,150]
+p2 = [150,20]
+o2 = [20 , 100]
+r = [0,0]
+#cv2.line(img , p1[0:2] , o1[0:2] ,(0,0,0),3)
+#scv2.line(img , p2[0:2] , o2[0:2] ,(0,0,0),3)
+cv2.imshow("Hos", img)
+x  = intersection(p1,o1,p2,o2,r)
+print(x)
+cv2.waitKey(0)
